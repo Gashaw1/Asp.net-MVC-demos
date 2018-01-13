4678458e942d06a04c1bd53799917f03494ea29d
Gashaw1 <akalc2004@yahoo.com>
2018-01-12 20:19:12 -0500

    .

--- Consume_GoogleBooks_API_in_MVC/GoogleBooks/Models/VolumeInfo.cs
+++ Consume_GoogleBooks_API_in_MVC/GoogleBooks/Models/VolumeInfo.cs
@@ -5,6 +5,12 @@ using System.Web;
 
 namespace GoogleBooks.Models
 {
+    public class Result
+    {
+        string kind { get; set; }
+        string TotlaItem { get; set; }
+        public Items _Items { get; set; }
+    }
     public class Items
     {
         string Kind { get; set; }
@@ -16,11 +22,59 @@ namespace GoogleBooks.Models
     public class VolumeInfo
     {
         public string Title { get; set; }
-        public Authors _Author { get; set; }
+        public List<Authors> _Author { get; set; }
         public string Publisher { get; set; }
-        public string PublisDate { get; set; }    
-        public string Description { get; set; }   
+        public string PublisDate { get; set; }
+        public string Description { get; set; }
+        public List<IndustryIdentifier> industryIdentifiers { get; set; }
+        public ReadingModes _ReadingModes { get; set; }
+        public int PageCount { get; set; }
+        public string PrintType { get; set; }
+        public List<Catagorie> Catagories { get; set; }
+        public string MaturityRating { get; set; }
+        public bool alowAnonLogging { get; set; }
+        public string contentVersion { get; set; }
+        public List<string> ImagesLinks { get; set; }
+        public string Language { get; set; }
+        public string PreviewLink { get; set; }
+        public string InfoLink { get; set; }
+        public string canonicalVolumeLink { get; set; }
+        public SaleInfo _SaleInfo { get; set; }
+        public AccessInfo _AccessIno { get; set; }
+        public SearchInfo _serarchInfo { get; set; }
+
     }
+
+    public class SearchInfo
+    {
+        public string textSnipet { get; set; }
+    }
+
+    public class AccessInfo
+    {
+        public string country { get; set; }
+        public string ViewAbility { get; set; }
+        public bool emeddable { get; set; }
+        public bool publicDomain { get; set; }
+        public string textToSpeechPermission { get; set; }
+        public Epub epub { get; set; }
+        public Pdf pdf { get; set; }
+        public string ReaderLink { get; set; }
+        public string AccessViewStatus { get; set; }
+        public bool quteSharingAllowed { get; set; }
+
+    }
+
+    public class Pdf
+    {
+        public bool Isabaliable { get; set; }
+    }
+
+    public class Epub
+    {
+        public bool IsAvaliable { get; set; }
+    }
+
     public class Authors
     {
         public List<string> authors { get; set; }
@@ -28,12 +82,22 @@ namespace GoogleBooks.Models
     public class IndustryIdentifier
     {
         public List<string> types { get; set; }
-        public List<string> identifiers { get; set; }   
+        public List<string> identifiers { get; set; }
     }
     public class ReadingModes
     {
         public bool text { get; set; }
         public bool image { get; set; }
+    }
+    public class Catagorie
+    {
+        public List<string> catagory { get; set; }
+    }
+    public class SaleInfo
+    {
+        public string country { get; set; }
+        public string Saleability { get; set; }
+        public bool IsEbook { get; set; }
     }
 
 }
